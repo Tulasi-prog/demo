6424c49eb6966bf6c9ed0367173f8e93c44552dd
Tulasi-prog <tulasihmtulasihm@gmail.com>
2025-03-13 15:36:20 +0530

    project 8

---
+++ demo/src/main/java/com/Tulasi-prog/App.java
@@ -0,0 +1,13 @@
+package com.Tulasi-prog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
